123ffeacd28da7d3a5b4e3fa10c4cda55a493aa0
Carlos Ebbers <cel.ebbers@gmail.com>
2022-11-18 14:31:36 -0300

    first commit

---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/labpadroesprojetospring/service/ViaCepService.java
@@ -0,0 +1,16 @@
+package one.digitalinnovation.labpadroesprojetospring.service;
+
+//Client Http criado por OpenFeign p consumo de API do ViaCEP
+
+import one.digitalinnovation.labpadroesprojetospring.model.Endereco;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "viacep", url="https://viacep.com.br/ws")
+public interface ViaCepService {
+    @GetMapping("/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+}
---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/labpadroesprojetospring/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,70 @@
+package one.digitalinnovation.labpadroesprojetospring.service.impl;
+
+import one.digitalinnovation.labpadroesprojetospring.model.Cliente;
+import one.digitalinnovation.labpadroesprojetospring.model.ClienteRepository;
+import one.digitalinnovation.labpadroesprojetospring.model.Endereco;
+import one.digitalinnovation.labpadroesprojetospring.model.EnderencoRepository;
+import one.digitalinnovation.labpadroesprojetospring.service.ClienteService;
+import one.digitalinnovation.labpadroesprojetospring.service.ViaCepService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+    // singleton: injetar os componentes do spring com @Autowired
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private EnderencoRepository enderecoRepository;
+
+    @Autowired
+    private ViaCepService viaCepService;
+
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+
+        return clienteRepository.findAll();
+    }
+
+    // Strategy: Implementar os metodo definidos na interface
+    // Facade: Abstrair integracoes com subsistemas, provendo interface simples
+    @Override
+    public Cliente buscarPorId(Long id) {
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        return cliente.get();
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+        salvarClienteComCep(cliente);
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+        Optional<Cliente> clienteBd = clienteRepository.findById(id);
+        if(clienteBd.isPresent()) {
+            salvarClienteComCep(cliente);
+        }
+    }
+
+    private void salvarClienteComCep(Cliente cliente) {
+        String cep = cliente.getEndereco().getCep();
+        Endereco endereco = enderecoRepository
+                .findById(cep)
+                .orElseGet(() -> {
+                    Endereco novoEndereco = viaCepService.consultarCep(cep);
+                    enderecoRepository.save(novoEndereco);
+                    return novoEndereco;
+                });
+        cliente.setEndereco(endereco);
+        clienteRepository.save(cliente);
+    }
+
+    @Override
+    public void deletar(Long id) {
+        clienteRepository.deleteById(id);
+    }
+}
